64cebbd3c2cef20f5fbbe3a95da37040414854d9
rafa <rafayelhtc@gmail.com>
2021-02-25 22:33:56 +1100

    table altered

--- FinalGitCrimeDB/FinalGitCrimeDB/dbo/Tables/DIMCity.sql
+++ FinalGitCrimeDB/FinalGitCrimeDB/dbo/Tables/DIMCity.sql
@@ -3,7 +3,7 @@
     [CityName]    NVARCHAR (100) NULL,
     [DateCreated] DATETIME       NULL,
     [DateExpired] DATETIME       NULL,
-    [StateID_FK]  INT            NULL,
+    [StateID_FK]  INT            NOT NULL,
     CONSTRAINT [PK_DIMCity] PRIMARY KEY CLUSTERED ([CityID] ASC)
 );
 
